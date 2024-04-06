5a7cbdf041539c9badd2f29d5a8e0dadc03e0720
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-04-06 23:06:57 +1100

    Update home configuration in axnix.nix

--- flake.nix
+++ flake.nix
@@ -27,87 +27,89 @@
   outputs = inputs @ { self, nixpkgs, disko, home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      user = "axite";
     in
     {
-      nixosConfigurations = {
-        ####################
-        # Installation ISO #
-        ####################
-        /*
+      nixosConfigurations =
+        {
+          ####################
+          # Installation ISO #
+          ####################
+          /*
         Build with the following command:
         nix build .#nixosConfigurations.ISO.config.system.build.isoImage
-        */
+          */
 
-        ISO = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ({ nixpkgs, ... }: {
-              systemd.services.sshd.wantedBy = nixpkgs.lib.mkForce [ "multi-user.target" ];
-              users.users.root.openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
+          ISO = lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              ({ nixpkgs, ... }: {
+                systemd.services.sshd.wantedBy = nixpkgs.lib.mkForce [ "multi-user.target" ];
+                users.users.root.openssh.authorizedKeys.keys = [
+                  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
+                ];
+              })
+              "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              ({ nixpkgs, ... }: {
+                environment.systemPackages = [ ];
+              })
+            ];
+          };
+
+
+          ################
+          # NixOS Systems#
+          ################
+
+          #### Notes #####
+          # Add the following to specify a disk: { disko.devices.disk.main = "/dev/sda"; }
+
+          axnix = lib.nixosSystem
+            {
+              system = "x86_64-linux";
+              specialArgs = { inherit inputs self user; };
+              modules = [
+                # home-manager
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager = {
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.axite = import ./home/axnix.nix;
+                    extraSpecialArgs = { inherit inputs self user; };
+                  };
+                }
+
+                # disko
+                disko.nixosModules.disko
+                {
+                  disko.devices.disk.main.device = "/dev/nvme0n1";
+                }
+
+                # Configuration
+                ./configuration.nix
+                ./hosts/axnix/default.nix
               ];
-            })
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-            ({ nixpkgs, ... }: {
-              environment.systemPackages = [ ];
-            })
-          ];
+            };
+
+          besta = lib.nixosSystem
+            {
+              system = "x86_64-linux";
+              modules = [
+                disko.nixosModules.disko
+                ./configuration.nix
+                ./hosts/besta/default.nix
+              ];
+            };
+          plex = lib.nixosSystem
+            {
+              system = "x86_64-linux";
+              modules = [
+                disko.nixosModules.disko
+                ./configuration.nix
+                ./hosts/plex/default.nix
+              ];
+            };
         };
-
-
-        ################
-        # NixOS Systems#
-        ################
-
-        #### Notes #####
-        # Add the following to specify a disk: { disko.devices.disk.main = "/dev/sda"; }
-
-        axnix = lib.nixosSystem
-          {
-            system = "x86_64-linux";
-            specialArgs = { inherit inputs self; };
-            modules = [
-              # home-manager
-              home-manager.nixosModules.home-manager
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.axite = import ./home/axnix.nix;
-                  extraSpecialArgs = { inherit inputs self; };
-                };
-              }
-              
-              # disko
-              disko.nixosModules.disko
-              {
-                disko.devices.disk.main.device = "/dev/nvme0n1";
-              }
-
-              # Configuration
-              ./configuration.nix
-              ./hosts/axnix/default.nix
-            ];
-          };
-
-        besta = lib.nixosSystem
-          {
-            system = "x86_64-linux";
-            modules = [
-              disko.nixosModules.disko
-              ./configuration.nix
-              ./hosts/besta/default.nix
-            ];
-          };
-        plex = lib.nixosSystem
-          {
-            system = "x86_64-linux";
-            modules = [
-              disko.nixosModules.disko
-              ./configuration.nix
-              ./hosts/plex/default.nix
-            ];
-          };
-      };
     };
 }
--- home/axnix.nix
+++ home/axnix.nix
@@ -1,80 +1,78 @@
+{ pkgs, ... }:
+let
+  username = "axite";
+in
 {
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  # Home Manager
+  # Home config
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    stateVersion = "24.05";
 
-  # axite user
-  home-manager.users.axite = { config, nixpkgs, ... }: {
-
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
-
-    # Home config
-    home = {
-
-      # State version
-      stateVersion = "24.05";
-
-      # Editor
-      sessionVariables = {
-        EDITOR = "codium";
-      };
-
-      # Packages
-      packages = with nixpkgs;
-        [
-          brave
-          #davinci-resolve # Big download :(
-          discord
-          dolphinEmu
-          gh
-          gimp
-          hunspell
-          k4dirstat
-          lutris
-          minecraft
-          neofetch
-          nixfmt
-          patchelf
-          protonup-qt
-          putty
-          spotify
-          unityhub
-          vlc
-          vscodium
-          wgnord
-        ];
+    # Editor
+    sessionVariables = {
+      EDITOR = "codium";
     };
 
-    # Programs
-    programs = {
+    # Packages
+    packages = with nixpkgs;
+      [
+        brave
+        #davinci-resolve # Big download :(
+        discord
+        dolphinEmu
+        gh
+        gimp
+        hunspell
+        k4dirstat
+        lutris
+        minecraft
+        neofetch
+        nixfmt
+        patchelf
+        protonup-qt
+        putty
+        spotify
+        unityhub
+        vlc
+        vscodium
+        wgnord
+      ];
+  };
 
-      # Bash
-      bash = {
-        enable = true;
-        initExtra = ''
-          . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-        '';
-      };
+  # Programs
+  programs = {
 
-      # Git
-      git = {
-        enable = true;
-        userName = "axite";
-        userEmail = "kylesmthh@gmail.com";
-      };
+    # Bash
+    bash = {
+      enable = true;
+      initExtra = ''
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      '';
+    };
 
-      # OBS
-      obs-studio = {
-        enable = true;
-        package = nixpkgs.obs-studio;
-        plugins =
-          map (plugin: nixpkgs.obs-studio-plugins.${plugin}) [
-            "obs-vaapi"
-            "obs-vkcapture"
-            "obs-gstreamer"
-            "wlrobs"
-          ];
-      };
+    # Git
+    git = {
+      enable = true;
+      userName = "axite";
+      userEmail = "kylesmthh@gmail.com";
+    };
+
+    # OBS
+    obs-studio = {
+      enable = true;
+      package = nixpkgs.obs-studio;
+      plugins =
+        map (plugin: nixpkgs.obs-studio-plugins.${plugin}) [
+          "obs-vaapi"
+          "obs-vkcapture"
+          "obs-gstreamer"
+          "wlrobs"
+        ];
     };
   };
 }
+
