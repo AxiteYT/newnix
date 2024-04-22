13e44e74620c6801debd9466d125176833843d48
axite <kylesmthh@gmail.com>
2024-04-22 18:53:09 +1000

    Modified axnix ssh key

--- hosts/munshi/default.nix
+++ hosts/munshi/default.nix
@@ -13,13 +13,16 @@
           home = "/home/munshi";
           description = "munshi User";
           extraGroups = [ "wheel" "networkmanager" ];
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
+          ];
         };
         nixdist = {
           isSystemUser = true;
           createHome = false;
           uid = 500;
           openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgVKHHxs6a+KyUkzQyEGMXbY+2nJ3kVe8KJMoPRxjgl root@axnix"
           ];
           group = "nixdist";
           useDefaultShell = true;
