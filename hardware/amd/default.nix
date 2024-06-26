eec4c4b776c953ecd8fe3bc65d99a1d6cdf3db71
axite <kylesmthh@gmail.com>
2024-06-26 19:16:59 +1000

    amd gpu update

--- hardware/amd/default.nix
+++ hardware/amd/default.nix
@@ -5,22 +5,6 @@
   # Set XServer/Wayland to use AMD
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  # OpenGL configuration
-  hardware.opengl = {
-    # Enable Driver
-    driSupport = true;
-    driSupport32Bit = true;
-
-    # Vulkan + OpenCL packages
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-      amdvlk
-    ];
-    extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
-  };
-
   # HIP Libraries override
   systemd.tmpfiles.rules =
     [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
