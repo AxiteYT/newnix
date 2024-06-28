4a55ef44710fcae3a4ec8f6936bdeb7afa87acfc
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-06-28 10:07:17 +1000

    feat: Update qbittorrent configuration with data directory, user, group, and port settings

--- modules/qbittorrent/default.nix
+++ modules/qbittorrent/default.nix
@@ -1,6 +1,18 @@
-{ pkgs, nixflk, ... }: {
+{ pkgs, nixflk, ... }:
+let
+  qbittorrentModule = nixflk + "/modules/services/torrent/qbittorrent.nix";
+in
+{
+  imports = [
+    qbittorrentModule
+  ];
+
   services.qbittorrent = {
     enable = true;
     openFirewall = true;
+    dataDir = "/var/lib/qbittorrent";
+    user = "qbittorrent";
+    group = "servarr";
+    port = 8080;
   };
 }
