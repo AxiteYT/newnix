ac5528add2048d5dab2df40541817eefef323d73
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-06-28 11:17:08 +1000

    chore: Remove wireguard and wgnord system packages from default.nix

--- hosts/besta/default.nix
+++ hosts/besta/default.nix
@@ -18,9 +18,4 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINMXEwWst3Kkag14hG+nCtiRX8KHcn6w/rUeZC5Ww7RU axite@axitemedia.com"
     ];
   };
-
-  environment.systemPackages = with pkgs; [
-    wgnord
-    wireguard-tools
-  ];
 }
