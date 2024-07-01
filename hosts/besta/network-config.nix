2def3f93c9191f25afb274f7ca95c8edc1740e15
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-01 17:43:31 +1000

    chore: Update network-config.nix with iptables rules for VPN connection

--- hosts/besta/network-config.nix
+++ hosts/besta/network-config.nix
@@ -17,10 +17,22 @@
         address = [ "10.2.0.2/32" ];
         dns = [ "10.2.0.1" ];
         privateKeyFile = "/root/.wg/besta-AU-1.key";
+        postUp = ''
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -A FORWARD -o wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.2.0.2/32 -o enp6s18 -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -A INPUT -i enp6s18 -s 192.168.1.0/24 -j ACCEPT
+        '';
+        preDown = ''
+          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -D FORWARD -o wg0 -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.2.0.2/32 -o enp6s18 -j MASQUERADE
+          ${pkgs.iptables}/bin/iptables -D INPUT -i enp6s18 -s 192.168.1.0/24 -j ACCEPT
+        '';
         peers = [{
           publicKey = "8kyi2e0ziUqhs+ooJYYI0yaVhv/bneUC1fhV5X2q/SE=";
           endpoint = "138.199.33.236:51820";
-          allowedIPs = [ "0.0.0.0/0" ];
+          allowedIPs = [ "0.0.0.0/0" "192.168.1.0/24" ];
         }];
       };
     };
