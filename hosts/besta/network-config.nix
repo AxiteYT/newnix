6454d8f252e9fa5894545ec40bb65cfcd809c34b
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-02 08:49:06 +1000

    Added postRules for local traffic

--- hosts/besta/network-config.nix
+++ hosts/besta/network-config.nix
@@ -1,9 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  wggateway = "10.2.0.1";
+in
+{
 
   # WG Application
-  environment.systemPackages = with pkgs; [
-    wireguard-tools
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      wireguard-tools
+    ];
 
   # Networking Configuration
   networking = {
@@ -22,13 +27,24 @@
     wg-quick.interfaces = {
       wg0 = {
         address = [ "10.2.0.2/32" ];
-        dns = [ "10.2.0.1" ];
+        dns = [ ${wggateway} ];
         privateKeyFile = "/root/.wg/besta-AU-1.key";
         peers = [{
           publicKey = "8kyi2e0ziUqhs+ooJYYI0yaVhv/bneUC1fhV5X2q/SE=";
           endpoint = "138.199.33.236:51820";
           allowedIPs = [ "0.0.0.0/0" ];
         }];
+        # PostUp
+        postUp = ''
+          ip rule add from 192.168.1.0/24 table main prio 10
+          ip rule add not from 192.168.1.0/24 table 1234 prio 20
+          ip route add default via ${wggateway} dev wg0 table 1234
+        '';
+        # PostDown
+        postDown = ''
+          ip rule delete from 192.168.1.0/24 table main prio 10
+          ip rule delete not from 192.168.1.0/24 table 1234 prio 20
+        '';
       };
     };
   };
