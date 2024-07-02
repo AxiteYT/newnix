510996d050b7a6d1bbfd828419f7b95be513bd8c
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-02 16:40:46 +1000

    Add routes fore wireguard interface

--- hosts/besta/network-config.nix
+++ hosts/besta/network-config.nix
@@ -10,6 +10,22 @@
         address = "192.168.1.85";
         prefixLength = 24;
       }];
+      wg0 = {
+        ipv4.routes = [
+          {
+            address = "192.168.1.0";
+            prefixLength = 24;
+          }
+          {
+            address = "10.1.20.0";
+            prefixLength = 24;
+          }
+          {
+            address = "10.1.30.0";
+            prefixLength = 24;
+          }
+        ];
+      };
     };
   };
 }
