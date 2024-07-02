c7cdb78cfdcaf06bdf36dff14fe74518785c0803
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-02 10:17:41 +1000

    chore: Update network-config.nix to include local subnet variable and improve postUp and postDown rules

--- hosts/besta/network-config.nix
+++ hosts/besta/network-config.nix
@@ -5,21 +5,28 @@ let
   localgateway = "192.168.1.1";
 in
 {
+
   # WG Application
-  environment.systemPackages = with pkgs; [
-    wireguard-tools
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      wireguard-tools
+    ];
 
   # Networking Configuration
   networking = {
+    #Hostname
     hostName = "besta";
 
+    # Interface
     interfaces = {
       enp6s18.ipv4.addresses = [{
         address = "192.168.1.85";
         prefixLength = 24;
       }];
+    };
 
+    # WireGuard VPN Configuration
+    wg-quick.interfaces = {
       wg0 = {
         address = [ "10.2.0.2/32" ];
         dns = [ "${wggateway}" ];
@@ -27,17 +34,18 @@ in
         peers = [{
           publicKey = "8kyi2e0ziUqhs+ooJYYI0yaVhv/bneUC1fhV5X2q/SE=";
           endpoint = "138.199.33.236:51820";
-          allowedIPs = [ "0.0.0.0/0" ]; # All traffic through VPN
+          allowedIPs = [ "0.0.0.0/0" ];
         }];
 
-        #postUp
+        # postUP
         postUp = ''
           ip rule add from ${localsubnet} lookup main prio 10
           ip rule add not from ${localsubnet} lookup 1234 prio 20
           ip route add default via ${localgateway} dev enp6s18 table main
           ip route add default via ${wggateway} dev wg0 table 1234
         '';
-        #postDown
+
+        # postDown
         postDown = ''
           ip rule del from ${localsubnet} lookup main prio 10
           ip rule del not from ${localsubnet} lookup 1234 prio 20
