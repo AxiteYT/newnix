ba0f73f018b452b8d8e9c053538fbccf01f328fa
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-02 22:55:24 +1000

    feat: Add netbootxyz-efi package to systemPackages

--- modules/netbootxyz/default.nix
+++ modules/netbootxyz/default.nix
@@ -1,4 +1,9 @@
-{
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs;[
+    netbootxyz-efi
+  ];
+
+  # Netboot XYZ Configuration
   boot.loader.systemd-boot.netbootxyz = {
     enable = true;
   };
