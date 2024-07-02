a2d13ef938d29b739ec0d8c3eba6dd7908b18942
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-02 10:06:10 +1000

    Modify postRules

--- hosts/besta/network-config.nix
+++ hosts/besta/network-config.nix
@@ -36,17 +36,19 @@ in
           allowedIPs = [ "0.0.0.0/0" ];
         }];
 
-        # PostUp
+        # postUP
         postUp = ''
+          ip route add ${localsubnet} dev enp6s18 proto static scope link table main
           ip rule add from ${localsubnet} table main prio 10
           ip rule add not from ${localsubnet} table 1234 prio 20
           ip route add default via ${wggateway} dev wg0 table 1234
         '';
 
-        # PostDown
+        # postDown
         postDown = ''
-          ip rule delete from ${localsubnet} table main prio 10
-          ip rule delete not from ${localsubnet} table 1234 prio 20
+          ip route del ${localsubnet} dev enp6s18 table main
+          ip rule del from ${localsubnet} table main prio 10
+          ip rule del not from ${localsubnet} table 1234 prio 20
         '';
       };
     };
