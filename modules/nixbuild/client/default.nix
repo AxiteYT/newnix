e795b5ad1a2d86294e4c95d8a1cb5b9b2ec38f47
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-12 18:52:51 +1000

    Nix builder re-init

--- modules/nixbuild/client/default.nix
+++ modules/nixbuild/client/default.nix
@@ -1,43 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  /*
-    # Setup Nix to use the remote builder
-    nix = {
-    distributedBuilds = true;
-    buildMachines = [{
-      hostName = "192.168.1.7";
-      sshUser = "builduser";
-      system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 20;
-      speedFactor = 5;
-      supportedFeatures = [ "big-parallel" "benchmark" ];
-    }];
-    settings = {
-      trusted-users = [
-        "root"
-        "builduser"
-        "@wheel"
-      ];
-    };
-    };
-  */
-  # SSH client configuration for root, assuming root is used for builds
-  programs.ssh = {
-    extraConfig = ''
-      Host 192.168.1.7
-      Hostname 192.168.1.7
-      User builduser
-      IdentityFile /root/.ssh/nix_builder_key
-      IdentitiesOnly yes
-      StrictHostKeyChecking accept-new
-    '';
-  };
-
-  # Generate SSH keys for root, manage manually if security policies vary
-  # This can be managed outside of Nix for greater control
-  users.users.root = {
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCjeX/x/7ZS4gKFmTHf29FTVcA7hjrZciNjkJxMPlhRBnt20jvj8Xyy4hu+OipYogAPv1k1DDgXiNgZ+Eb2xaomko7o9GWcoUOOGbD7LEjisyaL2C7LvoFzKIJeQJ87Dk12eaPD41Vjd/sD7oTqWDig6pfRovRfdXjO3oE3M4igRkis8Y7jWHVIyi7cD3jbb2X+QPQmCZLmsMiFvj/6J1r9unDS7jW0vERI7404NxhXLQTwwVv4t86ka4Rcs4EiDmWquBQ5sLQg7iVn/XPmShw/RluGh+eJDEUITsG5+d9QrickozFRL87gnfVI3Pbm0Nc6DfN8KFhiaveNebGoUZY34K8w+lbQHYrXT+j/Dh5N6+HNO97eq1zf+Auv5HLLo13nP/dS+uqhC+kC6aZqTKL7tSAnRCc717Ua1C32fSdXK5H5vZmeD+BFGpekBSbLiEz4J1+vfXO4A9bu9wono7m0gkV+WqAB4OJ6n/AMa7G0R48bHwUTRRg7JIkEknL9ljLUlreGJWyT5q484sWE4Tg/PsAsQMMYpO2h3uz5J6dMlnEjYHNxspPD9D5UZjyw3tJ8Ifbliqz/fPD2cTGxkz4dIEyHjWIhmvQrO3UNxS2gTrdSaC8/HpREtZWADIZfxz5cU2apLeuNyZPn3/UG8DkFOLhcU6qPePq0o3QFjbpdNw== nix-builder@axitemedia.com" ];
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--no-write-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
   };
 }
--- modules/nixbuild/server/default.nix
+++ modules/nixbuild/server/default.nix
@@ -1,41 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # Ensure SSH is enabled and properly configured
-  services.openssh = {
-    enable = true;
-    permitRootLogin = "no";
-    extraConfig = ''
-      AllowUsers builduser
-    '';
+  users.users.nixbuild = {
+    isNormalUser = false;
+    createHome = false;
+    extraGroups = [ "nixbld" ];
+    openssh.authorizedKeys.keys = [
+      "TODO: Add public key here"
+    ];
   };
 
-  # Setup user for builds
-  users.users.builduser = {
-    isNormalUser = true;
-    home = "/home/builduser";
-    createHome = true;
-    description = "Nix Build User";
-    extraGroups = [ "nixbld" ]; # Add to nixbld to allow Nix operations
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCjeX/x/7ZS4gKFmTHf29FTVcA7hjrZciNjkJxMPlhRBnt20jvj8Xyy4hu+OipYogAPv1k1DDgXiNgZ+Eb2xaomko7o9GWcoUOOGbD7LEjisyaL2C7LvoFzKIJeQJ87Dk12eaPD41Vjd/sD7oTqWDig6pfRovRfdXjO3oE3M4igRkis8Y7jWHVIyi7cD3jbb2X+QPQmCZLmsMiFvj/6J1r9unDS7jW0vERI7404NxhXLQTwwVv4t86ka4Rcs4EiDmWquBQ5sLQg7iVn/XPmShw/RluGh+eJDEUITsG5+d9QrickozFRL87gnfVI3Pbm0Nc6DfN8KFhiaveNebGoUZY34K8w+lbQHYrXT+j/Dh5N6+HNO97eq1zf+Auv5HLLo13nP/dS+uqhC+kC6aZqTKL7tSAnRCc717Ua1C32fSdXK5H5vZmeD+BFGpekBSbLiEz4J1+vfXO4A9bu9wono7m0gkV+WqAB4OJ6n/AMa7G0R48bHwUTRRg7JIkEknL9ljLUlreGJWyT5q484sWE4Tg/PsAsQMMYpO2h3uz5J6dMlnEjYHNxspPD9D5UZjyw3tJ8Ifbliqz/fPD2cTGxkz4dIEyHjWIhmvQrO3UNxS2gTrdSaC8/HpREtZWADIZfxz5cU2apLeuNyZPn3/UG8DkFOLhcU6qPePq0o3QFjbpdNw== nix-builder@axitemedia.com" ];
-  };
+  
 
-  # Configure Nix Daemon to allow remote builds
-  nix = {
-    distributedBuilds = true;
-    buildMachines = [{
-      hostName = "localhost";
-      system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 20;
-      speedFactor = 10;
-    }];
-    settings = {
-      trusted-users = [
-        "root"
-        "builduser"
-        "@wheel"
-      ];
-    };
-  };
 }
