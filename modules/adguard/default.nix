d5d39f2a56d4fdc666982d4389ca1b70c24b484b
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-26 12:17:59 +1000

    Add AdGuard Home firewall configuration

--- modules/adguard/default.nix
+++ modules/adguard/default.nix
@@ -1,7 +1,14 @@
 {
+  # AdGuard Home
   services.adguardhome = {
     enable = true;
     openFirewall = true;
     port = 3000;
+  };
+
+  # Firewall config
+  config.networking.firewall = {
+    allowedTCPPorts = [ 3000 ];
+    allowedUDPPorts = [ 53 ];
   };
 }
