d91557fe43508a57dacac4bfa436b1458c19ba83
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-07-26 12:22:14 +1000

    re-format

--- modules/adguard/default.nix
+++ modules/adguard/default.nix
@@ -1,18 +1,18 @@
 { pkgs, lib, ... }:
 {
-  # AdGuard Home
-  services.adguardhome = {
-    enable = true;
-    openFirewall = true;
-    port = 3000;
-  };
-
-  # Firewall config
   config = {
     networking = {
       firewall = {
         allowedTCPPorts = [ 3000 ];
         allowedUDPPorts = [ 53 ];
+      };
+    };
+
+    services = {
+      adguardhome = {
+        enable = true;
+        openFirewall = true;
+        settings.bind_port = 3000;
       };
     };
   };
