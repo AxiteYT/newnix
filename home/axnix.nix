5779d8065b2a1f4ad22e5b4b44b43e755fb779f6
axite <kylesmthh@gmail.com>
2024-09-07 09:11:17 +1000

    Remove alacritty formatting

--- home/axnix.nix
+++ home/axnix.nix
@@ -85,21 +85,9 @@ in
       enable = true;
 
       settings = {
-        window = {
-          title = "Terminal";
-
-          padding = {
-            y = 5;
-          };
-          dimensions = {
-            lines = 75;
-            columns = 100;
-          };
-        };
 
         font = {
           normal.family = "JetbrainsMono Nerd Font";
-          size = 8.0;
         };
 
         background_opacity = 0.3;
