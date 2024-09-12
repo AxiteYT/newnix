b0e748963ed34dd789d790d0de8cf1e05d723f38
axite <kylesmthh@gmail.com>
2024-09-12 20:41:15 +1000

    Add flaresolverr

--- modules/servarr/default.nix
+++ modules/servarr/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ../qbittorrent/default.nix
     ./bazarr/default.nix
+    ./flaresolverr.default.nix
     ./prowlarr/default.nix
     ./radarr/default.nix
     ./sonarr/default.nix
