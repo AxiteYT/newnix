3e0ffefd491fef8fbddd473fb9f6ee1aa4169302
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-09-26 23:16:44 +1000

    deluge config

--- modules/deluge/default.nix
+++ modules/deluge/default.nix
@@ -2,10 +2,17 @@
   services.deluge = {
     enable = true;
     group = "servarr";
+    declarative = true;
+    dataDir = "/media/Plex/Downloads";
     authFile = "/etc/nixos/deluge.auth";
     web = {
       enable = true;
       openFirewall = true;
+    };
+    config = {
+      enabled_plugins = [ "Label" "Scheduler" ];
+      outgoing_interface = "wg0";
+      listen_interface = "wg0";
     };
   };
 }
