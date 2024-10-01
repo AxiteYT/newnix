6be9899372363adbec48d02140b90fcdeb9a8a0a
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-10-01 18:37:04 +1000

    Fixups to kernels

--- hardware/amd/default.nix
+++ hardware/amd/default.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 {
+  # Enable GPU Drivers
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      amdvlk
+    ];
+    extraPackages32 = with pkgs; [
+      driversi686Linux.amdvlk
+    ];
+  };
+
   # Set GPU in kenel
   boot.initrd.kernelModules = [ "amdgpu" ];
 
