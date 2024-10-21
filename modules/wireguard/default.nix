bb321399db5b10d9fa9a79e82ae80d0d49a926a3
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-10-21 12:19:19 +1100

    NAT-PMP Service for Port Mapping

--- modules/wireguard/default.nix
+++ modules/wireguard/default.nix
@@ -7,8 +7,8 @@
   ];
 
   # WireGuard VPN Configuration
-  networking.wg-quick.interfaces = {
-    wg0 = {
+  networking = {
+    wg-quick.interfaces.wg0 = {
       address = [ "10.2.0.2/32" ];
       dns = [ "10.2.0.1" ];
       privateKeyFile = "/root/.wg/besta-AU-1.key";
@@ -39,40 +39,40 @@
         ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT ! -o wg0 -m mark --mark 0 -j MARK --set-xmark 0
       '';
     };
+
+    firewall = {
+      allowedTCPPorts = [ 26504 ];
+      allowedUDPPorts = [ 26504 ];
+    };
   };
 
-  # NAT-PMP Port Mapping Service
+  # NAT-PMP Service for Port Mapping
   systemd.services.natpmp = {
-    description = "NAT-PMP Port Mapping Renewal Service";
+    description = "NAT-PMP Port Mapping Service";
     after = [
-      "network.target"
+      "network-online.target"
       "wg-quick-wg0.service"
     ];
-    wants = [ "wg-quick-wg0.service" ];
+    wants = [
+      "network-online.target"
+      "wg-quick-wg0.service"
+    ];
+    requires = [ "wg-quick-wg0.service" ];
 
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.bash}/bin/bash -c '\
-        previous_port=" "; \
+        ${pkgs.libnatpmp}/bin/natpmpc -a 26504 26504 udp 60 -g 10.2.0.1; \
+        ${pkgs.libnatpmp}/bin/natpmpc -a 26504 26504 tcp 60 -g 10.2.0.1; \
         while true; do \
-          output=$(${pkgs.libnatpmp}/bin/natpmpc -g 10.2.0.1 -a 1 0 tcp 60); \
-          echo \"$output\"; \
-          allocated_port=$(echo \"$output\" | grep \"Mapped public port\" | grep \"tcp\" | awk \"{print \$5}\"); \
-          echo \"Allocated port: $allocated_port\"; \
-          # Remove previous firewall rule \
-          if [ ! -z \"$previous_port\" ]; then \
-            ${pkgs.iptables}/bin/iptables -D INPUT -p tcp --dport $previous_port -j ACCEPT; \
-          fi; \
-          # Add new firewall rule \
-          ${pkgs.iptables}/bin/iptables -I INPUT -p tcp --dport $allocated_port -j ACCEPT; \
-          # Update previous_port \
-          previous_port=$allocated_port; \
-          # Write the allocated port to a file \
-          echo \"$allocated_port\" > /var/run/allocated_port; \
-          chmod 644 /var/run/allocated_port; \
-          sleep 45; \
+            date; \
+            ${pkgs.libnatpmp}/bin/natpmpc -a 26504 26504 udp 60 -g 10.2.0.1 && \
+            ${pkgs.libnatpmp}/bin/natpmpc -a 26504 26504 tcp 60 -g 10.2.0.1 || { \
+                echo -e \"ERROR with natpmpc command \a\"; \
+                break; \
+            }; \
+            sleep 45; \
         done'";
-
       Restart = "always";
       RestartSec = 5;
     };
