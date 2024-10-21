cdc609482a23d10fc747dd5e1510f8dca7171408
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-10-21 11:13:24 +1100

    post changes

--- modules/wireguard/default.nix
+++ modules/wireguard/default.nix
@@ -23,21 +23,21 @@
 
       # postRules to allow localtraffic
       postUp = ''
-        wg set wg0 fwmark 51820 
-        ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT -o wg0 -m mark --mark $(wg show wg0 fwmark) -j ACCEPT
+        wg set wg0 fwmark 51820
+        ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT -o wg0 -m mark --mark 51820 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT ! -o wg0 -m addrtype --dst-type LOCAL -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT ! -o wg0 -d 192.168.1.0/24 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT ! -o wg0 -d 10.1.1.0/24 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT ! -o wg0 -m mark --mark 0 -j MARK --set-xmark $(wg show wg0 fwmark)
+        ${pkgs.iptables}/bin/iptables -t mangle -A OUTPUT ! -o wg0 -m mark --mark 0 -j MARK --set-xmark 51820
+        ${pkgs.libnatpmp}/bin/natpmpc -a 1 0 tcp 26504 -g 10.2.0.1
       '';
+
       postDown = ''
-        ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT -o wg0 -m mark --mark $(wg show wg0 fwmark) -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT -o wg0 -m mark --mark 51820 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT ! -o wg0 -m addrtype --dst-type LOCAL -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT ! -o wg0 -d 192.168.1.0/24 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT ! -o wg0 -d 10.1.1.0/24 -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t mangle -D OUTPUT ! -o wg0 -m mark --mark 0 -j MARK --set-xmark 0
-
-        ${pkgs.libnatpmp}/bin/natpmpc -a 1 0 tcp 26504 -g 10.2.0.1
       '';
     };
   };
