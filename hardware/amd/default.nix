3a46e0663fce753378c08cc9117d739233c5b84c
axite <kylesmthh@gmail.com>
2024-10-29 22:29:59 +1100

    Remove all the extra from amd drivers

--- hardware/amd/default.nix
+++ hardware/amd/default.nix
@@ -5,30 +5,11 @@
     graphics = {
       enable = true;
       enable32Bit = true;
-
-      # Enable OpenCL
-      extraPackages = with pkgs; [ rocmPackages.clr.icd ];
     };
   };
 
-  # HIP Config
+  # LACT
   systemd = {
-    tmpfiles.rules =
-      let
-        rocmEnv = pkgs.symlinkJoin {
-          name = "rocm-combined";
-          paths = with pkgs.rocmPackages; [
-            rocblas
-            hipblas
-            clr
-          ];
-        };
-      in
-      [
-        "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
-      ];
-
-    # LACT
     packages = with pkgs; [ lact ];
     services.lactd.wantedBy = [ "multi-user.target" ];
   };
