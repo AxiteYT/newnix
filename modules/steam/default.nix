20f72d69c6dc2ea8bd6838e953ba5c12d7ace74c
Kyle Smith <60371141+AxiteYT@users.noreply.github.com>
2024-11-19 17:44:53 +1100

    added lib + use wiki config

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.steam = {
     enable = true;
-
-    # Networking
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 }
