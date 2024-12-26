34cb32dd3bb1cdb9979f6a055c4d01b0031ce6e2
axite <kylesmthh@gmail.com>
2024-12-26 13:45:02 +1100

    Enable bluetooth

--- hosts/axnix/default.nix
+++ hosts/axnix/default.nix
@@ -54,4 +54,16 @@
     };
   };
   hardware.pulseaudio.enable = false;
+
+  # Enable Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+      };
+    };
+  };
+  services.blueman.enable = true;
 }
