0ea1bb3c7f6e046152d3fbc74da334357c0f01a4
Kyle Smith <axite@axitemedia.com>
2025-06-04 14:08:07 +1000

    some hyprland

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
   # Enable hyprland
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
 
   # Set electron apps to use hyprland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
+  # Required packages
   environment.systemPackages = with pkgs; [
     kitty
   ];
